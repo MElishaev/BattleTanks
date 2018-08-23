827277eeef51c1bad9b85f54d3cab79338cb9f28
mark <secret678@gmail.com>
2018-08-23 14:55:49 +0300

    Added crosshair location on the screen in pixels

--- BattleTanks/Source/BattleTanks/TankPlayerController.cpp
+++ BattleTanks/Source/BattleTanks/TankPlayerController.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankPlayerController.h"
+#include "Engine/World.h"
 
 void ATankPlayerController::BeginPlay()
 {
@@ -16,9 +17,66 @@ void ATankPlayerController::BeginPlay()
 		UE_LOG(LogTemp, Warning, TEXT("The ControlledTank is %s"), *ControlledTank->GetName());
 }
 
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	AimAtCrosshair();
+}
+
 ATank * ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
+
+void ATankPlayerController::AimAtCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+
+	FVector HitLocation; // Out parameter
+
+	if (GetSightRayHitLocation(HitLocation))
+		UE_LOG(LogTemp, Warning, TEXT("The crosshair hitting: %s"), *HitLocation.ToString());
+		// TODO tell the controlled tank to aim at that point
+}
+
+// get world location where the linetrace intersects the landscape, TRUE if hits landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitResult) const
+{
+	// Getting screen size - why?
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	// Pixel coordinates of the crosshair
+	FVector2D ScreenLocation = FVector2D(ViewportSizeX*CrosshairXLocation, ViewportSizeY*CrosshairYLocation);
+	
+	return false;
+
+
+
+	//FHitResult Hit;
+	//// Get View point of the player each frame 
+	//FRotator PlayerViewPointRotation;
+	//FVector PlayerViewPointVector;
+	//GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(PlayerViewPointVector, PlayerViewPointRotation);
+	//UE_LOG(LogTemp, Warning, TEXT("PlayerViewPointRotation: %s	PlayerViewPointVector: %s"), *PlayerViewPointRotation.ToString(), *PlayerViewPointVector.ToString());
+
+	//// Linetrace from player view for later use on objects in reach
+	//FVector End = PlayerViewPointVector + PlayerViewPointRotation.Vector() * 10000.f;
+
+	//FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+	//GetWorld()->LineTraceSingleByObjectType(
+	//	Hit,
+	//	PlayerViewPointVector,
+	//	End,
+	//	FCollisionObjectQueryParams(ECollisionChannel::ECC_WorldStatic),
+	//	TraceParameters
+	//);
+	//// get the first landscape coordinates it hits
+	//OutHitResult = Hit.Location;
+	//UE_LOG(LogTemp, Warning, TEXT("HitResult is: %s"), *OutHitResult.ToString());
+
+	//return OutHitResult.IsZero();
+}
+
 
 
--- BattleTanks/Source/BattleTanks/TankPlayerController.h
+++ BattleTanks/Source/BattleTanks/TankPlayerController.h
@@ -16,7 +16,24 @@ class BATTLETANKS_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
+	void BeginPlay() override;
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
 	ATank * GetControlledTank() const;
 	
-	void BeginPlay() override;
+	// Aim the barrel to the point where the linetrace of the crosshair hits the landscape
+	void AimAtCrosshair();
+
+private:
+	// Crosshair location
+	UPROPERTY(EditAnywhere)
+	float CrosshairXLocation = 0.5;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairYLocation = 0.333333;
+
+	// Get the Point where the linetrace hit the landscape
+	bool GetSightRayHitLocation(FVector&) const;
 };
