e02d5860c1b9ccfd5bb65f8c50c2545011f4c0d2
mark <secret678@gmail.com>
2019-04-01 21:21:14 +0300

    Moved dependency of AIController from Tank to AimingComponent

--- BattleTanks/Source/BattleTanks/TankAIController.cpp
+++ BattleTanks/Source/BattleTanks/TankAIController.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankAIController.h"
-#include "Tank.h"
+#include "TankAimingComponent.h"
 #include "GameFramework/PlayerController.h"
 #include "Engine/World.h"
 
@@ -14,15 +14,16 @@ void ATankAIController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	auto AIControlledTank = Cast<ATank>(GetPawn());
-	auto PlayerTank = Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+	auto AIControlledTank = GetPawn();
+	auto PlayerTank = GetWorld()->GetFirstPlayerController()->GetPawn();
 	
-	if (ensure(PlayerTank))
-	{
-		MoveToActor(PlayerTank, AcceptanceRadius);
+	if (!ensure(PlayerTank && AIControlledTank)) { return; }
 
-		AIControlledTank->AimAt(PlayerTank->GetTargetLocation());
+	MoveToActor(PlayerTank, AcceptanceRadius);
 
-		AIControlledTank->Fire(); // TODO: limit firing rate
-	}
+	auto AimingComponent = AIControlledTank->FindComponentByClass<UTankAimingComponent>();
+	AimingComponent->AimAt(PlayerTank->GetTargetLocation());
+
+	// AIControlledTank->Fire(); // TODO: limit firing rate
+
 }
